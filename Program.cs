0f499f1f468ffeac9220a32b522373667107dc5c
ChClaude <christ.tchambila@gmail.com>
2021-03-21 23:35:01 +0200

    first commit

---
+++ Program.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Collections.Generic;
+using Newtonsoft.Json.Linq;
+
+namespace DotnetChallenge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WebRequest request = WebRequest.Create("https://coderbyte.com/api/challenges/json/json-cleaning");
+
+            request.ContentType = "application/json; charset=utf-8";
+
+            WebResponse response = request.GetResponse();
+
+            // Console.WriteLine(response.ToString());
+
+            string dataString = "";
+
+            using (var stream = new StreamReader(response.GetResponseStream()))
+            {
+                dataString = stream.ReadToEnd();
+            }
+
+            JObject data = JObject.Parse(dataString);
+
+            Console.WriteLine("==================================== Raw Object ============================");
+            Console.WriteLine(data);
+
+            var invalidKeys = new List<string>();
+
+            // foreach (var x in data)
+            // {
+            //     string name = x.Key;
+            //     JToken value = x.Value;
+
+            //     if (value.GetType() == typeof(JObject))
+            //     {
+            //         // the object is a JObject
+            //         // var stringValue = value.Value.ToString();
+            //         // if(string.IsNullOrEmpty(stringValue) || stringValue.Equals("-") ||
+            //         //     stringValue.Equals("N/A"))
+            //         // {
+            //         //     value
+            //         // }
+            //     }
+            //     else if (value.GetType() == typeof(JValue))
+            //     {
+            //         // the object is a JValue
+            //         if(string.IsNullOrEmpty(value.ToString()) || value.ToString().Equals("-") ||
+            //             value.ToString().  Equals("N/A"))
+            //         {
+            //             invalidKeys.Add(name);
+            //             // value.Remove();
+            //         }
+            //     }
+            //     else if (value.GetType() == typeof(JArray))
+            //     {
+            //         // Console.WriteLine(value);
+
+            //         foreach (var item in value)
+            //         {
+            //             Console.WriteLine(value.GetType());
+            //         }
+
+            //         // the object is a JArray
+            //     }
+
+            //     // Console.WriteLine(value.GetType());
+            // }
+
+            // foreach(var key in invalidKeys) {
+            //     data.Remove(key);
+            // }
+
+            Console.WriteLine("==================================== Output ============================");
+            Console.WriteLine(CleanJObject(data));
+            // Console.WriteLine(data);
+
+            response.Close();
+        }
+
+
+
+
+        public static JObject CleanJObject(JObject MyJObject)
+        {
+            var invalidKeys = new List<string>();
+
+            foreach (var x in MyJObject)
+            {
+                string name = x.Key;
+                JToken value = x.Value;
+
+                if (value.GetType() == typeof(JObject))
+                {
+                    CleanJObject(((JObject) value));
+                }
+                else if (value.GetType() == typeof(JValue))
+                {
+                    // the object is a JValue
+                    if(string.IsNullOrEmpty(value.ToString()) || value.ToString().Equals("-") ||
+                        value.ToString().  Equals("N/A"))
+                    {
+                        invalidKeys.Add(name);
+                    }
+                }
+                else if (value.GetType() == typeof(JArray))
+                {
+                    var invalItems = new List<JToken>();
+
+                    // the object is a JArray
+                    foreach(var item in value)
+                    {
+                        string stringValue = item.ToString();
+
+                        if (string.IsNullOrEmpty(stringValue) || stringValue.ToString().Equals("-") ||
+                        stringValue.ToString().  Equals("N/A"))
+                        {
+                            invalItems.Add(item);
+                        }
+                    }
+
+                    foreach(var item in invalItems)
+                    {
+                        ((JArray) value).Remove(item);
+                    }
+
+                    // Console.WriteLine(value.GetType());
+                }
+
+            }
+
+            foreach(var key in invalidKeys)
+            {
+                    MyJObject.Remove(key);
+            }
+
+            return MyJObject;
+        }
+    }
+}
